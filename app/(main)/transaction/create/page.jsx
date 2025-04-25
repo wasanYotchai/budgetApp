d7385bc319ef27494a9c68616b9f76873e283c35
wasanYotchai <wayotchai@gmail.com>
2025-04-25 10:59:35 -0500

    Update page.jsx

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -16,7 +16,7 @@ export default async function AddTransactionPage({ searchParams }) {
   return (
     <div className="max-w-3xl mx-auto px-5">
       <div className="flex justify-center md:justify-normal mb-8">
-        <h1 className="text-5xl gradient-title "> {editId ? "Edit" : "Add"} Transaction</h1>
+        <h1 className="text-5xl gradient-title ">Add Transaction</h1>
       </div>
       <AddTransactionForm
         accounts={accounts}
