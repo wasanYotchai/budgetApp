d6ce2c9fc6409f59d8c72feaf03b576c3f830ce8
wasanYotchai <wayotchai@gmail.com>
2025-04-24 23:10:46 -0500

    Update page.jsx

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import { getUserAccounts } from '@/actions/dashboard';
-import AddTransactionForm from '../_components/transaction-form';
-import { defaultCategories } from '@/data/categories';
+import { getUserAccounts } from "@/actions/dashboard";
+import { defaultCategories } from "@/data/categories";
+import { AddTransactionForm } from "../_components/transaction-form";
+import { getTransaction } from "@/actions/transaction";
 
-const AddTransactionPage = async () => {
-    const accounts = await getUserAccounts();
+export default async function AddTransactionPage({ searchParams }) {
+  const accounts = await getUserAccounts();
+  const editId = searchParams?.edit;
 
-  
-    return (
+  let initialData = null;
+  if (editId) {
+    const transaction = await getTransaction(editId);
+    initialData = transaction;
+  }
+
+  return (
     <div className="max-w-3xl mx-auto px-5">
-      <div className="flex justify-center md:justify-normal mb-8"></div>
-
-            <h1 className="text-5xl gradient-title ">Add Transaction</h1>
-
-          <AddTransactionForm
-            accounts={accounts}
-            categories={defaultCategories}
-
-          />
-        </div>
-      );
-    }
-export default AddTransactionPage; 
+      <div className="flex justify-center md:justify-normal mb-8">
+        <h1 className="text-5xl gradient-title "> {editId ? "Edit" : "Add"} Transaction</h1>
+      </div>
+      <AddTransactionForm
+        accounts={accounts}
+        categories={defaultCategories}
+        editMode={!!editId}
+        initialData={initialData}
+      />
+    </div>
+  );
+}
